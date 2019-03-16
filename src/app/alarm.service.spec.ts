66eef9bac7ed5f53b056a55e0d85db0b32d1bc06
Chris Velevitch <chris.velevitch@gmail.com>
2019-03-16 23:00:57 +0800

    Play sound when the timer stops.

--- src/app/alarm.service.spec.ts
+++ src/app/alarm.service.spec.ts
@@ -3,10 +3,25 @@ import { TestBed } from '@angular/core/testing';
 import { AlarmService } from './alarm.service';
 
 describe('AlarmService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    jasmine.clock().uninstall();
+    jasmine.clock().install();
+  });
 
+  afterEach(() => {
+    jasmine.clock().uninstall();
+  });
   it('should be created', () => {
     const service: AlarmService = TestBed.get(AlarmService);
     expect(service).toBeTruthy();
   });
+
+  it('audio played', () => {
+    const service: AlarmService = TestBed.get(AlarmService);
+    const mySpy = spyOn(service, 'play');
+    service.play();
+    expect(mySpy).toHaveBeenCalled();
+  });
+
 });
